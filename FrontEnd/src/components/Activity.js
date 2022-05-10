c00ee87331925626689710fd391218d10aca018d
Ligank <Arthur.Josselin@hotmail.fr>
2022-05-10 16:23:36 +0200

    début

---
+++ FrontEnd/src/components/Activity.js
@@ -0,0 +1,9 @@
+import './../styles/Activity.css';
+
+function Activity() {
+    return <div className="Activity">
+            
+    </div>
+}
+
+export default Activity
---
+++ FrontEnd/src/components/Food.js
@@ -0,0 +1,20 @@
+import '../styles/Food.css';
+
+function Food() {
+    return <div className="Food">
+        <div className='food_boxes'>
+
+        </div>  
+        <div className='food_boxes'>
+
+        </div> 
+        <div className='food_boxes'>
+
+        </div> 
+        <div className='food_boxes'>
+
+        </div>           
+    </div>
+}
+
+export default Food
